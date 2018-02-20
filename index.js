302a0d2a41b966565d649ebdb050ad11d940852c
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2018-02-19 23:01:13 -0300

    implementada logica de buscar presente

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
-const mongo = require('mongodb').MongoClient
-const urlDb = 'mongodb://localhost:27017/'
-
-
+const mongodb = require('mongodb')
+const mongo = mongodb.MongoClient
+// const urlDb = 'mongodb://localhost:27017/'
+const urlDb = 'mongodb+srv://marcaosi:marcao1996@cluster0-bj0ai.mongodb.net/test'
 
 app.listen(3000, (err) => {
     console.log('Server running...')
@@ -24,17 +24,67 @@ app.set('view engine', 'ejs')
 //     next()
 // })
 
+app.get('/gift', (req, res) => {
+    res.render('formGift')
+})
+
+app.post('/gift', (req, res) => {
+    let gift = req.body
+    gift.status = 'Disponível'
+
+    mongo.connect(urlDb, (err, db) => {
+        if(!err){
+            const dbo = db.db('meucasamento')
+            dbo.collection('gifts').insert(gift)
+            res.redirect('/')
+        }else{
+            console.log("Impossível conectar no banco de dados => \n" + err)
+        }
+    })
+})
+
+app.get('/markGift/:id', (req, res) => {
+    mongo.connect(urlDb, (err, db) => {
+        if(!err){
+            const dbo = db.db('meucasamento')
+            let id = new mongodb.ObjectID(req.params.id)
+            dbo.collection('gifts').find({_id:id}).toArray((err, result) => {
+                if(err){
+                    res.status(500).send('erro'+err)
+                }else{
+                    // console.log(result)
+                    let gift = result[0]
+                    if(gift){
+                        res.render('markGift', {
+                            gift
+                        })
+                    }else{
+                        res.redirect('/')
+                    }
+                    
+                }
+            })
+        }else{
+            console.log("Impossível conectar no banco de dados => \n" + err)
+        }
+    })
+})
+
 app.get('/', (req, res) => {
     mongo.connect(urlDb, (err, db) => {
-        const dbo = db.db('meucasamento')
-        dbo.collection('gifts').find({}).toArray((err, result) => {
-            if(err){
-                res.status(500).send('erro')
-            }else{
-                res.render('index', {
-                    gifts: result
-                })
-            }
-        })
+        if(!err){
+            const dbo = db.db('meucasamento')
+            dbo.collection('gifts').find({}).toArray((err, result) => {
+                if(err){
+                    res.status(500).send('erro')
+                }else{
+                    res.render('index', {
+                        gifts: result
+                    })
+                }
+            })
+        }else{
+            console.log("Impossível conectar no banco de dados => \n" + err)
+        }
     })
 })
